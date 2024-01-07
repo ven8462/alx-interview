2543907858486e3ce7864d886ed478246ba46fb5
lavender <lavenderanyango9@gmail.com>
2024-01-07 10:26:57 +0300

    making function corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-""" module containing the pascal triangle"""
+"""pascals never ending tree"""
+
 
 def pascal_triangle(n):
-    """
-    a function that returns a list of lists of integers
-    representing the Pascal’s triangle
-    """
-    # Check if n is less than or equal to 0
-    if n <= 0:
-        return []
-
-    # empty list to store the rows of Pascal's Triangle
+    """this method draws pascals triangle given an integer n"""
     triangle = []
 
-    # Iterate through each row from 0 to n-1
-    for i in range(n):
-        # Create a new row with i + 1 elements
-        # all initialized to 1
-        row = [1] * (i + 1)
-
-        # Check if the current row is not one of the first two rows
-        if i > 1:
-            # Iterate through the interior elements of the row
-            for j in range(1, i):
-                # Calculate the interior elements by summing element
-                # from the previous row
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-
-        # Append the current row to the triangle list
-        triangle.append(row)
-
-    # Return the final Pascal's Triangle
+    if (n <= 0):
+        return triangle
+    for i in range(1, n + 1):
+        arr = []
+        for j in range(1, i + 1):
+            # corner conditions
+            if i <= 2 or j == i or j == 1:
+                val = 1
+            else:
+                val = triangle[i - 2][j - 1] + triangle[i - 2][j - 2]
+            arr.append(val)
+        triangle.append(arr)
     return triangle
