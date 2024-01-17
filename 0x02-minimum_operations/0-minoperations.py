0b4bce3b774cc7080be3c456a889d7335571a73f
lavender <lavenderanyango9@gmail.com>
2024-01-17 15:43:00 +0300

    adding comments

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,15 +3,30 @@
 
 
 def minOperations(n):
-    """ Minimum Operations """
+    """
+    Calculates the minimum number of operations required to
+    reach a given number 'n' by performing only two operations:
+    - Copy All: Copy the current string.
+    - Paste: Paste the copied string.
+
+    Args:
+        n (int): The target number.
+
+    Returns:
+        int: The minimum number of operations required.
+    """
+    # Check if the target number is less than or equal to 1
     if n <= 1:
         return 0
-    x = 2
-    y = 0
-    while x <= n:
-        if n % x == 0:
-            y += x
-            n = n / x
+
+    x = 2  # Initialize x to 2
+    y = 0  # Initialize y to 0
+
+    while x <= n:  # Loop until x is less than or equal to the target number
+        if n % x == 0:  # Check if the target number is divisible by x
+            y += x  # Add x to y
+            n = n / x  # Update the target number by dividing it by x
         else:
-            x += 1
-    return y
+            x += 1  # Increment x by 1
+
+    return y  # Return the minimum number of operations required
